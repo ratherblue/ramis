cb1b26304e2dc8d3484b9c3cc8e78f815a6c2763
ratherblue <ratherblue@gmail.com>
2016-06-08 11:28:31 -0700

    test fix

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ var server = http.createServer(handleRequest);
 
 server.listen(8080, function() {
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT); //eslint-disable-line no-console
+    console.log("Server listening on: http://localhost:%s", PORT); // eslint-disable-line no-console
 });
