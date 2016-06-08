2b09ef37f14776fc77388e627d2dad308c383004
ratherblue <ratherblue@gmail.com>
2016-06-08 11:27:29 -0700

    initial files

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var PORT = 8080;
+
+function handleRequest(request, response) {
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(8080, function() {
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT); //eslint-disable-line no-console
+});
