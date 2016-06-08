74cb3de76289fb4a0acc55ca6f4d70685607a130
ratherblue <ratherblue@gmail.com>
2016-06-08 13:31:44 -0700

    fix port, add procfile for Heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var PORT = 80;
+var PORT = 8080;
 
 function handleRequest(request, response) {
     response.end('It Works!! Path Hit: ' + request.url);
@@ -8,7 +8,7 @@ function handleRequest(request, response) {
 
 var server = http.createServer(handleRequest);
 
-server.listen(PORT, function() {
+server.listen({ port: PORT }, function() {
     // Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT); // eslint-disable-line no-console
 });
