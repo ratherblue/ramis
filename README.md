b09a3a12bc038456ea2dd65274d76417d2aaff39
ratherblue <ratherblue@gmail.com>
2016-06-08 11:31:06 -0700

    add build shield

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # elastic
+
+[![Build Status](https://img.shields.io/travis/ratherblue/elastic.svg?style=flat-square)](https://travis-ci.org/ratherblue/elastic)
--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function handleRequest(request, response) {
 var server = http.createServer(handleRequest);
 
 server.listen(8080, function() {
-    //Callback triggered when server is successfully listening. Hurray!
+    // Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT); // eslint-disable-line no-console
 });
