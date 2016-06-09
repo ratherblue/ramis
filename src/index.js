cb198fb8c9b3ada0e1520fe649c37bb514d52796
ratherblue <ratherblue@gmail.com>
2016-06-09 15:41:04 -0700

    fix linting errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-var express = require('express');
-var app = express();
+var express = require('express'); var app = express();
 var path = require('path');
 var https = require('https');
 
 var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.get('/', function(request, response) {
-    response.render(path.join(__dirname,'views', 'index'));
+    response.render(path.join(__dirname, 'views', 'index'));
 });
 
 app.post('/event-handler', function(request) {
     var payload = '';
+
     request.on('data', function(chunk) {
         payload += chunk.toString();
     });
@@ -33,7 +34,8 @@ app.post('/event-handler', function(request) {
         var options = {
             protocol: 'https:',
             host: 'api.github.com',
-            path: '/repos/ratherblue/ramis/statuses/' + sha + '?access_token=' + process.env.GITHUB_AUTH_TOKEN,
+            path: '/repos/ratherblue/ramis/statuses/' +
+                sha + '?access_token=' + process.env.GITHUB_AUTH_TOKEN,
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -44,9 +46,6 @@ app.post('/event-handler', function(request) {
 
         var req = https.request(options, function(res) {
             res.setEncoding('utf8');
-            // res.on('data', function (chunk) {
-            //     console.log('body: ' + chunk);
-            // });
         });
 
         req.write(postData);
@@ -54,9 +53,26 @@ app.post('/event-handler', function(request) {
     });
 });
 
+app.get('/oauth', function(request, response) {
+    var params = [
+        'client_id=368d97fad7902db3554c',
+        'redirect_uri=http://localhost:8080/oauth-success',
+        'scope=public_repo'
+    ];
+
+    var url = 'https://github.com/login/oauth/authorize?' + params.join('&');
+
+    response.redirect(url);
+});
+
+app.get('/oauth-success', function(request, response) {
+    response.render(path.join(__dirname, 'views', 'success'));
+});
+
 
 app.listen(port, function() {
-    console.log('Running on http://localhost:' + port); // eslint-disable-line no-console
+    // eslint-disable-next-line no-console
+    console.log('Running on http://localhost:' + port);
 });
 
 exports.init = function() {
